330e95a92e04b4d6cea210825f4a9680f0f56861
Alexis Kofman <alexis.kofman@gmail.com>
2014-07-28 23:08:23 +0200

    Init project

---
+++ server.js
@@ -0,0 +1,60 @@
+'use strict'
+
+var config = require('./config');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var Twit = require('twit');
+var T = new Twit({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token: config.access_token,
+    access_token_secret: config.access_token_secret
+});
+var io = require('socket.io')(http);
+
+app.use(express.static('.'));
+
+io.on('connection', function(socket) {
+    var stream;
+
+    console.log(socket.id + ' - New socket connected');
+    socket.on('filter', function(filter) {
+        if (stream) {
+            stream.stop();
+            console.log(socket.id + ' - Stream stopped');
+        }
+
+        if (typeof filter === 'string') {
+            stream = T.stream('statuses/filter', {
+                track: filter
+            });
+
+            console.log(socket.id + ' - Tracking tweets filtered by ' + filter + '...');
+            stream.on('tweet', function(tweet) {
+                socket.emit('tweet', tweet);
+                console.log(tweet.text);
+            });
+
+            stream.on('error', function(error) {
+                console.log(error.message);
+            });
+        }
+    });
+
+    socket.on('disconnect', function() {
+        console.log(socket.id + ' - Socket disconnected');
+        if (stream) {
+            stream.stop();
+            console.log(socket.id + ' - Stream stopped');
+        }
+    });
+});
+
+app.get('/twitterSearch', function(req, res) {
+    res.sendfile('twitterSearch.html');
+});
+
+http.listen(8080, function() {
+    console.log('listening on *:8080');
+});
---
+++ twitterSocketStreamController.js
@@ -0,0 +1,49 @@
+angular.module('twitterSocketStreamApp', ['btford.socket-io', 'ngAnimate'])
+    .controller('twitterSocketStreamController', ['$scope', '$http', 'socketFactory',
+        function($scope, $http, socketFactory) {
+
+            var myIoSocket;
+            var socketId;
+
+            myIoSocket = io({
+                'forceNew': true
+            });
+
+            myIoSocket.on('connect', function() {
+                socketId = myIoSocket.io.engine.id;
+                console.log(socketId + " - Socket connected");
+            });
+
+            var mySocket = socketFactory({
+                ioSocket: myIoSocket
+            });
+
+            mySocket.on('disconnect', function() {
+                console.log(socketId + " - Socket disconnected");
+            });
+
+            mySocket.on('tweet', function(tweet) {
+                if ($scope.tweets) {
+                    //console.log(tweet);
+                    $scope.tweets.push({
+                        authorImage: tweet.user.profile_image_url,
+                        author: tweet.user.screen_name,
+                        text: tweet.text,
+                        date: tweet.created_at.split(' ')[3]
+                    });
+                }
+            });
+
+            $scope.search = function(filter) {
+                if (filter) {
+                    $scope.tweets = [];
+                    mySocket.emit('filter', filter);
+                }
+            };
+        }
+    ]).filter('reverse', function() {
+        return function(tweets) {
+            if (!angular.isArray(tweets)) return false;
+            return tweets.slice().reverse();
+        };
+    });
